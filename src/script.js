cb5b5a17483ac527d67417cacdb420f2999f86cd
Andrey Shtarev <54120707+Shtarev@users.noreply.github.com>
2020-05-08 19:39:19 +0200

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,119 @@
+﻿var arr = [];
+var str = '';
+var string = "abbeißen Affenhitze Bullenhitze Wasserhahn Eselsbrücke Drahtesel Sparfuchs Lackaffe Weber Weihnacht Weisheit weiterhin Weltmarkt wenigstens Wüste Mannschaft meistens mittleren mutmaßliche zugänglich zukünftige zwingend abgelehnt aktivieren Ambiente anderem angegriffen Anregung Ansprüche Anwender attraktive aufrufen ausfallen ausgeübt Ausweg Flughafen fördern freiwillige Fußgänger Qualität Räuber Rechnung rechtzeitig regelmäßig Ruhestand überblick übrigens unheimlich Unterlagen Utopie Leistungen linken Auch verzaubern vereinbaren vertraut schon lange keinem Politiker mehr keiner Wahl Rassist aber Ausländer bekämen viel Geld deshalb Brexit Tatsächlich legen Veröffentlich Armut Ungleichheit wichtiger Faktor Referendum gewesen könnten Dabei könnten Männer großen Verlierer dwerden Absturz britischen Pfunds infolge Austritt Referendums Angaben von schon jetzt Lebenshaltung kosten Menschen ährlich Pfund erhöht Autokonzern Nissan Jaguar wollen wegen Unsicherheit Produktion Teil andere Länder verlagern Tausende Menschen werden Jobs verlieren Verarmung befeuere weiteren Aufstieg extremen Rechten dahin würden Menschen wenden keine anderen Optionen sehen Dabei hätte Bankencrash durchaus Alternativen gegeben groß Britannien hätte anstatt sparen eigene Land investieren können Alston kommt riesiger Laborversuch unnachgiebig Spar Programm gepeitscht worden Labor lassen Ursache Wirkung millionen fachen Menschen Versuch genau beobachten Sonder Berichter statt Vereinten Nationen extreme Armut Jährlich zwei Länder die besucht Vergangenes Jahr reiste Wochen sprach Sozialarbeit Lehrern Frauen ihrer Not heraus prostituier seinem Abschlussbericht davor derzeitige Kurs hinführt lieh dafür Worte britischen Philosophen schrieb über Leben einer Gesellschaft ohne funktionierenden Gesellschaftsvertrag menschliche Leben ist einsam armselig ekelhaft tierisch kurz Junge Schwierigkeiten Schule neidet anderen Kindern Klamotten Spielzeuge Kurzem angefangen klauen Thomas seinen Sohn einen Weg einschlagen Ende Knast Tod stehen schon oft gesehen persönlichen Umfeld Viertel Polsterbett Doppelbett Einzelbett Sofa Liegesofa Liege Polster Hocker Stuhl Arbeitsstuhl Rollstuhl Sessel Arbeitssessel Tisch Esstisch Couchtisch Rundtisch Schreibtisch Toilettentisch Computertisch Küchentisch Arbeitstisch Serviertisch Teppich Teppichboden Teppichboden Bücherregal Schrank Kleiderschrank Geschirr Sekretär Kommode Schrankwand Entsprechend Ablauf Countdowns manchen Kreisen Enttäuschung anderen Erleichterung Zwar brach Server sofort Last Zugriffe zusammen Seite Magazin Royale pünktlich dreizehn Langfassung ersehnten Neuigkeit Lied Europa orchestriert Böhmermann wechselweise vorgetragen Satirikern Niederlanden Frankreich Litauen Portugal Ungarn Luxemburg Norwegen Kroatien Finnland Zeichen Zeiten besonders dunkel schlägt Stunde gackernden Außenseiter Spaßmacher anarchische Figur durchaus weißs Macht Gelächter größte Gefahr Elite vorgibt Ernst Lage widmen notwendiger weise rechts unbedingt links einfach Bananen Schale Mächtigen ausrutschen wollen Politik Agonie manövriert Pointe befreien loszuwerden cheidenpilz Symptome Pilzinfektion wirklich abgeklungen Zusätzlich helfen folgende Tipp schnell handeln sollten sie schnell handeln scheiden Pilz Infektion Schwimmbad Handtücher unterlegen Schwimmbad Handtücher unterlegen Land üblichen Medikamenten baumwollen Wäsche bevorzugen zahlreiche Schutz Barriere Entspannung Phasen Schwanger Schaft Woche Mutterpass eingetragen Proportion Übersicht Übersicht Europawahl stimm berechtigt Rechte Populisten verlieren Information konstruktiv Misstrauen votum Grundgesetz anmutigen Gemüt Verfassung derselben Hoffnung frohen Verabschieden krassesten Darstellung Menschlichkeit vorkommen Rechte Star Deutung unmöglich starr gleichberechtigt sondern Zauberspruch Prominenz beeindruckt Zentrum deutschen Eliten Bürgern zugemutet Generation Besonderheit Monat abrechnung Wöchentlich aktuellen Bestseller finden Hörbücher Ratgeber Außenseiter Versuchungen widersteht chinesische Verband versucht Boxer Ausschluss Maßstäbe Wende gedruckten Häuser entstehen bedürftige Menschen drängendes Problem zusammen Autobiografie berichtet Musiker Vergangenheit kennengelernt aufeinander treffen Demnach gewesen widersprochen seiner Darstellung nun Begegnung Anmerkung Redaktion unangenehm anfühlt Besiegen Inhalte Kostenloser Report sofort verfügbar Blutzucker Spiegel Diabetes bekämpfen Lebensführen zurück drängen Weg Heilung Spaß Exemplar natürlich Anzeige Angebot Tag Euro schnell Wohnraum gedruckten Häuser Drucker Chance auf Gewinne Teil Erlöse viele gemeinnützige Partne Gemüse Ei verantwortlich offenbar bereits sicher Massive Gespräch Manipulation versuche über durch Russland können Dennoch darauf mit Erleichterung populistische bestellen und gegen Establishment gerichtete beobachtete Niveau Desinformation originell manipulierten Online Seufzer Frühstück Regal Beruf Normalität akzeptieren Gefahr übrigens Leichter Anstieg Facebook Twitter Stimmen Benzin Bemühungen Sprachen Männer Aussage Fakten Eltern Meinung Demokratien debattieren geschlechtslos verheiratet Erdbeere Helfer sondern Beleidigung Frühjahr Flasche lautete englische Antwort Assistenten Frage Filme Musik Fotostrecke Tatsächlich haben bekanntesten Assistenten allesamt standardmäßig Haushalt weiblich klingende Stimmen Tendenz vorgeben Veröffentlich Internet Nutzer Bilder Suggerieren bestimmten Geschlechts oft Frauen gefügige oder gefallen süchtig Problemen Jetzt definiert was normal Nach Einschätzung Nutzer Geräte schreiben Autoren Stimme digitalen Thema Muster Praxis proben Vorlagen immer griffbereit jetzt warm wahr voll trocken Schinken Löffel Gewürz spülen Käse Gewürz Pfeffer Abendessen Tomate Fehlschlag Rückschlag Flop Pechvogel abnehmend minderwertig Mönch Rhabarber Jachtschoner Büstenhalter Schreibtisch Lagerei Gewerbes Düsseldorf Termin Lösung ehemalig Vorwurf diskret Mangel einsilbig verstohlen Abwiegen plaudern imaginär Entführer Skizze Kuckuck einordnen Besen nachweisen chaotisch Ausrede Perverser wertvoll Augenblick peinlich demütigen empören Pferde deuten unheilbar begleiten Verfügung seufzen Reihe beneiden Stunde Gespräch Fischer ersehnte erhalten Verdacht voreilig angeblich Kapazität hinzufügen reglos Entsetzen stumm besinnen beleidigen Begegnung Bedingun finster forsch eifrig schräg happig Besitz Nötigste Notwendig Vergnügen wahrhaft Aufregung benehmen flehen Würde verraten verderben begeistern ratlos Zuneigung Gerücht Ruck Elend Einfluss erledigen verkünden Hebamme entbinden hegen schwül behindern wütend zornig Hunger aufgeregt erwischen auswendig bestechen umgehend zurück winken dauerhaft leiblich befristen schimpfen stottern Rätsel leuchten schnappen munter trocken schreiben beherrschen gelingen authentisch erstaunt Ohren Klingel auffalten Branche duzen verhalten Neid blendende Laune allerbeste lau beweisen Ofen frech Pfand überlegen gelangen geraten dahinkommen Wahnsinn Zettel gestehen schämen Schlucker Folter erschöpfen ungeschickt träge retten Schwindler malochen vernünftig erschöpft horchen Narbe observieren heiter feindlich nirgends saumäßig Einigung Einwand Wende boshaft kitschig niesen adrett Betäubung verknallen motzig meckern Gespür Gehorsam nuscheln kurzum verlässig eben haften entweder Ausleihe verwegen Belastet bevorsteht erkunden eigentlich";
+arr = string.split(' ');
+str = arr[getRandomInt(0, arr.length)];
+
+var strSuffle = shufstr(str);
+var i = strSuffle.length;
+
+for(var i=0; i<strSuffle.length; i++){
+	var div = document.createElement('div');
+	div.id = i+'_box';
+	div.classList.add('box');
+	div.innerHTML = strSuffle.charAt(i);
+	div.setAttribute('draggable', true);
+	div.setAttribute('ondragstart', 'dragstart(event)');
+	div.setAttribute('ondragend', 'dragend(event)');
+	blockContainer.appendChild(div);
+}
+
+for(var i=0; i<str.length; i++){
+	var div = document.createElement('div');
+	div.id = str.charAt(i)+'_block';
+	div.classList.add('block');
+	div.setAttribute('ondragover', 'allowDrop(event)')
+	div.setAttribute('ondrop', 'drop(event, this)');
+	div.setAttribute('ondragleave', 'dragleave(event)');
+	boxContainer.appendChild(div);
+} 
+
+function shufstr(str){
+	var shufstr = '';
+	while(str.length){
+		var b = Math.floor(Math.random()*(str.length));
+		shufstr = shufstr + str.slice(b, b+1);
+		str = str.slice(0, b) + str.slice(b+1);
+	}
+	return shufstr;
+}
+
+var boxId = '';
+var lit = '';
+var lith = '';
+var effort = 0;
+
+function dragstart(event){
+	boxId = event.target.id;
+	lit = event.target.textContent;
+	setTimeout(function(){
+		event.target.style.display = 'none';
+	},0);
+	
+}
+
+function dragend(event){ // ondragend
+	document.getElementById(boxId).style.display = '';
+}
+
+function allowDrop(event){
+	event.preventDefault();
+	event.target.classList.add('activ');
+	lith = event.target.id.charAt(0);
+}
+
+function dragleave(event){
+	event.preventDefault();
+	event.target.classList.remove('activ');
+	lith = event.target.id.charAt(0);
+}
+
+function drop(event, block){
+	if(lit == lith){
+		block.appendChild(document.getElementById(boxId));
+		event.target.classList.remove('activ');
+		document.getElementById(boxId).classList.add('starr');
+		document.getElementById(boxId).draggable = false;
+		i--;
+		if(!i){
+			go.style.display = 'block';
+		}
+	}
+	else {
+		event.target.classList.remove('activ');
+		effort++;
+		document.getElementById('effort').innerHTML = "Fehlschlag: "+effort;
+	}
+}
+
+function getRandomInt(min, max) {  
+    return Math.floor(Math.random() * (max - min + 1)) + min;  
+}
+
+////////////////////////////////////
+function size(){
+	if(document.documentElement.clientWidth >= document.documentElement.clientHeight){
+		document.getElementById('effortPos').style.top = document.documentElement.clientWidth/100*27+'px';
+		document.getElementById('effort').style.fontSize = document.documentElement.clientWidth/100*2.5+'px';
+	}
+	else {
+		document.getElementById('effortPos').style.top = document.documentElement.clientWidth/100*60+'px';
+		document.getElementById('effort').style.fontSize = document.documentElement.clientWidth/100*3.5+'px';
+	}
+}
+
+size();
+
+window.addEventListener('resize', function() {
+  size();
+}, false);
+
+var scr = window.matchMedia("all and (orientation:portrait)");
+
+scr.addListener(function(){
+	window.addEventListener('resize', function() {
+	  size();
+	}, false);
+});
+
+
